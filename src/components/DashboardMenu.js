175f2dd1af4becbece0ffb1c9750a6778df899d2
Rounak Polley <rounakpolley19972014@gmail.com>
2018-01-19 01:40:46 +0530

    implemented dashboard-menu ui  only on click events yet

---
+++ src/components/DashboardMenu.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import muiTheme from '../muiTheme.js';
+import Styles from '../Styles.js';
+import Paper from 'material-ui/Paper';
+import Menu from 'material-ui/Menu';
+import MenuItem from 'material-ui/MenuItem';
+import Divider from 'material-ui/Divider';
+
+class DashboardMenu extends Component{
+	constructor(props){
+		super(props);
+		const friends = [];
+		const groups = [];
+		var fKeys = Object.keys(this.props.friends);
+		var gKeys = Object.keys(this.props.groups);
+		var fLen = fKeys.length;
+		var gLen = gKeys.length;
+		for(let i=0; i<fLen; i++){
+			friends.push({
+				name: this.props.friends[fKeys[i]]
+			})
+		}
+		for(let j=0; j<gLen; j++){
+			groups.push({
+				name: this.props.groups[gKeys[j]]
+			})
+		}
+		this.state = {friends, groups};
+	}
+	render(){
+		return(
+			<Paper style={Styles.dashboardMenuList}>
+		      	<Menu desktop={true} width={180} listStyle={{'paddingBottom':'0px','paddingTop':'0px'}}>
+			        <MenuItem leftIcon={<i className="material-icons">dashboard</i>} primaryText="Dashboard" innerDivStyle={Styles.menuNav}/>
+			        <MenuItem leftIcon={<i className="material-icons">flag</i>} primaryText="Recent activity" innerDivStyle={Styles.menuNav}/>
+			        <MenuItem leftIcon={<i className="material-icons">list</i>} primaryText="All expenses" innerDivStyle={Styles.menuNav}/>
+			        <Divider />
+			        <MenuItem primaryText="GROUPS" secondaryText={<b>+ add</b>} innerDivStyle={Styles.menuDisabled} />
+			        <Divider style={{'marginTop':'0px'}}/>
+			        {	this.state.groups.map((groups, index) => (
+							<MenuItem leftIcon={<i className="material-icons">loyalty</i>} innerDivStyle={Styles.menuNav}
+							key={index} primaryText={groups.name}/>
+						))
+			        }
+			        <MenuItem primaryText="FRIENDS" secondaryText={<b>+ add</b>} innerDivStyle={Styles.menuDisabled} />
+			        <Divider style={{'marginTop':'0px'}}/>
+				    {	this.state.friends.map((friends, index) => (
+							<MenuItem leftIcon={<i className="material-icons">perm_identity</i>} innerDivStyle={Styles.menuNav}
+							key={index} primaryText={friends.name}/>
+						))
+			        }
+		    	</Menu>
+		    </Paper>
+		);
+	}
+}
+
+export default DashboardMenu;
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Styles from '../Styles.js';
+import DashboardMenu from '../components/DashboardMenu.js';
 
 class Dashboard extends Component{
     render(){
         return(
-            <div>
-                Dashboard
+            <div style={{display:'flex', flexDirection:'row', marginLeft:'200px'}}>
+                <DashboardMenu friends={this.props.friends}     groups={this.props.groups} />
+                Dashboard-center
             </div>
         );
     }
